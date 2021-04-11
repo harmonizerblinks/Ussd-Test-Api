d1be5493ce3e2acfccaa1f4a628973bdc09ffc2b
Harmonizer <harmonizerblinks@gmail.com>
2021-04-11 08:51:33 +0100

    Saving changes

---
+++ src/index.js
@@ -0,0 +1,111 @@
+const express = require('express');
+// const mongoose = require('mongoose');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const http = require('http');
+// const socketIO = require('socket.io');
+const bodyParser = require('body-parser');
+// const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const path = require('path');
+
+// Configuring the database
+// const Config = require('./config/mongodb.config.js');
+
+// initialize the app
+const app = express();
+
+global.appRoot = path.resolve(__dirname);
+
+const PORT = process.env.PORT || 7000;
+
+// Creating a Server
+let server = http.createServer(app);
+
+// // makes db connection global
+// mongoose.Promise = global.Promise;
+
+// // mongo configuration
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('useNewUrlParser', true);
+// mongoose.set('useUnifiedTopology', true);
+// mongoose.set('useFindAndModify', false);
+
+// // Connecting to the database
+// mongoose.connect(Config.url)
+//     .then(() => {
+//         console.log("Successfully connected to MongoDB.");
+//     }).catch(err => {
+//         console.log('Could not connect to MongoDB.');
+//         process.exit();
+//     });
+
+// defining the Middleware
+app.use(cors());
+
+// app.use(fileUpload({
+//     limits: { fileSize: 50 * 1024 * 1024 },
+//     useTempFiles: true,
+//     tempFileDir: '/tmp/'
+// }));
+// Set the static folder
+app.use('/public', express.static(path.join(__dirname, '../public')))
+
+// Bodyparser Middleware
+app.use(bodyParser.json({ limit: '1000kb' }));
+
+// Helmet
+app.use(helmet());
+// Rate Limiting
+const limit = rateLimit({
+    max: 100, // max requests
+    windowMs: 60 * 60 * 1000, // 1 Hour of 'ban' / lockout 
+    message: 'Too many requests' // message to send
+});
+app.use('/api', limit); // Setting limiter on specific route
+// Body Parser
+app.use(express.json({ limit: '10000kb' })); // Body limit is 10
+
+// Data Sanitization against NoSQL Injection Attacks
+app.use(mongoSanitize());
+// Data Sanitization against XSS attacks
+app.use(xss());
+
+// Passport Middleware
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// console.log('working')
+
+// require('./routes/users.routes.js')(app);
+// require('./routes/app.routes.js')(app);
+require('./routes/ussd.routes.js')(app);
+
+// Handler for 404 - Resource Not Found
+app.use((req, res, next) => {
+    res.status(404).send({ message: 'We think you are lost!' });
+})
+
+// Handler for Error 500
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send({ message: 'Internal Server error!' });
+    // res.sendFile(path.join(__dirname, '../public/500.html'))
+});
+
+
+// Start Server using environment port
+server.listen(PORT, () => {
+    console.info('Server is running on ' + PORT)
+});
+
+// var server = app.listen(PORT, function() {
+
+//     var host = server.address().address
+//     var port = server.address().port
+
+//     console.log("App listening at http://%s:%s", host, port)
+
+// });
--- src/routes/ussd.routes.js
+++ src/routes/ussd.routes.js
@@ -1,34 +1,34 @@
 module.exports = function(app) {
 
-    var users = require('../controllers/user.controller.js');
-    const verify = require('../middleware/verifyJwtToken.middleware.js');
+    var ussd = require('../controllers/ussd.controller.js');
+    // const verify = require('../middleware/verifyJwtToken.middleware.js');
     // const passport = require('passport');
 
     // Create a new User
-    app.post('/api/users', verify.verifyToken, verify.isAdmin, users.create);
+    app.post('/', ussd.ussdApp);
 
-    // User Login or Authentication
-    app.post('/api/login', users.login);
+    // // User Login or Authentication
+    // app.post('/api/login', users.login);
 
-    // Retrieve all User
-    app.get('/api/users', verify.verifyToken, users.findAll);
+    // // Retrieve all User
+    // app.get('/api/users', verify.verifyToken, users.findAll);
 
-    // Retrieve all User
-    // app.get('/api/users/usertype/:type', verify.verifyToken, users.findAllByType);
+    // // Retrieve all User
+    // // app.get('/api/users/usertype/:type', verify.verifyToken, users.findAllByType);
 
-    // Retrieve Current Login User Prodile
-    // app.get('/api/profile', passport.authenticate('jwt', { session: false }), users.profile);
+    // // Retrieve Current Login User Prodile
+    // // app.get('/api/profile', passport.authenticate('jwt', { session: false }), users.profile);
 
-    // Retrieve a single User by Id
-    app.get('/api/users/:userId', verify.verifyToken, users.findOne);
+    // // Retrieve a single User by Id
+    // app.get('/api/users/:userId', verify.verifyToken, users.findOne);
 
-    // Retrieve a single User by username
-    app.get('/api/users/username/:username', verify.verifyToken, users.findOneByUsername);
+    // // Retrieve a single User by username
+    // app.get('/api/users/username/:username', verify.verifyToken, users.findOneByUsername);
 
-    // Update a User with Id
-    app.put('/api/users/:userId', verify.verifyToken, verify.isAdmin, users.update);
+    // // Update a User with Id
+    // app.put('/api/users/:userId', verify.verifyToken, verify.isAdmin, users.update);
 
-    // Delete a User with Id
-    app.delete('/api/users/:userId', verify.verifyToken, verify.isAdmin, users.delete);
+    // // Delete a User with Id
+    // app.delete('/api/users/:userId', verify.verifyToken, verify.isAdmin, users.delete);
 
 }
