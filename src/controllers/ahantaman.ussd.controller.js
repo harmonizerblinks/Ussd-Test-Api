de6f164c5d66ba1c85b5a5907081010d629aadb9
Harmony Alabi <harmonizerblinks@gmail.com>
2021-06-02 08:43:22 +0000

    saving changes

--- src/controllers/ahantaman.ussd.controller.js
+++ src/controllers/ahantaman.ussd.controller.js
@@ -3,9 +3,9 @@ let menu = new UssdMenu({ provider: 'hubtel' });
 var unirest = require('unirest');
 let sessions = {};
 let types = ["", "Current", "Savings", "Susu" ];
-// let apiurl = "http://localhost:5000/Ussd/";
+let apiurl = "http://192.168.1.104:5000/Ussd/";
 // let apiurl = "https://api.alias-solutions.net:8444/MiddlewareApi/ussd/";
-let apiurl = "https://api.alias-solutions.net:5000/ussd/";
+// let apiurl = "https://api.alias-solutions.net:5000/ussd/";
 
 let access = { code: "ACU001", key: "1029398" };
 
