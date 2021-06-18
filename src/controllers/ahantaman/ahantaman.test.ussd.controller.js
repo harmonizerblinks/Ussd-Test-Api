d85fa35ce2dddaf20e09e509f6e5d252cb8f4d0c
Harmony Alabi <harmonizerblinks@gmail.com>
2021-06-18 13:40:26 +0000

    Updating changes

--- src/controllers/ahantaman/ahantaman.test.ussd.controller.js
+++ src/controllers/ahantaman/ahantaman.test.ussd.controller.js
@@ -8,8 +8,8 @@ let types = ["", "Current", "Savings", "Susu" ];
 // let apiurl = "https://app.alias-solutions.net:5000/ussd/";
 let apiurl = "https://app.alias-solutions.net:5003/ussd/";
 
-let access = { code: "ARB", key: "10198553" };
-// let access = { code: "ACU001", key: "1029398" };
+// let access = { code: "ARB", key: "10198553" };
+let access = { code: "ACU001", key: "1029398" };
 
 menu.sessionConfig({
     start: (sessionId, callback) => {
