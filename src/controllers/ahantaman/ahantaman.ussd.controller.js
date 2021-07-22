3f036f52c06a14c0054876c6f642bf722e17bfa7
Harmony Alabi <harmonizerblinks@gmail.com>
2021-07-22 20:30:10 +0000

    saving changes

--- src/controllers/ahantaman/ahantaman.ussd.controller.js
+++ src/controllers/ahantaman/ahantaman.ussd.controller.js
@@ -54,7 +54,7 @@ menu.startState({
         // Fetch Customer information
         console.log(menu.args);
         
-        // menu.end('Dear Customer, \nAhaConnect Service (*789*8#) is down for an upgrade. You will be notified when the service is restored. We apologise for any inconvenience.');
+        menu.end('Dear Customer, \nAhaConnect Service (*789*8#) is down for an upgrade. You will be notified when the service is restored. We apologise for any inconvenience.');
         await fetchCustomer(menu.args.phoneNumber, (data)=> { 
             console.log(1,data); 
             if(data.active && data.pin != '' && data.pin != null && data.pin != '1234') {
