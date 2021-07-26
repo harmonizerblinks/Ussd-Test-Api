a4915db2eebb6e63a8d758b8d36fb17ae6e6c1b9
Harmony Alabi <harmonizerblinks@gmail.com>
2021-07-26 12:03:27 +0000

    Adding Hmplus

--- src/controllers/hmplus/hmplus.test.ussd.controller.js
+++ src/controllers/hmplus/hmplus.test.ussd.controller.js
@@ -7,7 +7,7 @@ const config = require('../../config/mongodb.config.js');
 let types = ["", "Current", "Savings", "Susu" ];
 // let apiurl = "http://localhost:4000/Ussd/";
 // let apiurl = "https://api.alias-solutions.net:8444/MiddlewareApi/ussd/";
-let apiurl = "https://app.alias-solutions.net:5000/ussd/";
+let apiurl = "https://app.alias-solutions.net:5003/ussd/";
 
 let access = { code: "ARB", key: "10198553" };
 // let access = { code: "L005", key: "546787787" };
