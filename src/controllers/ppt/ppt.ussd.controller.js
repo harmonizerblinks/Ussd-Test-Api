ccd5fcfb37a277f293c515b691ae891c8a82718a
Harmony Alabi <harmonizerblinks@gmail.com>
2021-09-22 16:41:57 +0000

    saving changes

--- src/controllers/ppt/ppt.ussd.controller.js
+++ src/controllers/ppt/ppt.ussd.controller.js
@@ -8,14 +8,15 @@ let optionArray = [null, 'DAILY', 'WEEKLY', 'MONTHLY', null];
 const regex = /^[a-zA-Z]*$/;
 
 //Test Credentials
+let apiurl = "http://3e67-154-160-25-99.ngrok.io/ussd/";
 // let apiurl = "https://app.alias-solutions.net:5008/ussd/";
-// let apiSchemeInfo = "https://app.alias-solutions.net:5008/";
-// let access = { code: "446785909", key: "164383692" };
+let apiSchemeInfo = "https://app.alias-solutions.net:5008/";
+let access = { code: "446785909", key: "164383692" };
 
 //Live Credentials
-let apiurl = "https://app.alias-solutions.net:5009/ussd/";
-let apiSchemeInfo = "https://app.alias-solutions.net:5009/";
-let access = { code: "PPT", key: "178116723" };
+// let apiurl = "https://app.alias-solutions.net:5009/ussd/";
+// let apiSchemeInfo = "https://app.alias-solutions.net:5009/";
+// let access = { code: "PPT", key: "178116723" };
 
 menu.sessionConfig({
     start: (sessionId, callback) => {
