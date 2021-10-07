68d84d49deffea9d15c73d94e6ce11ba40531aa3
Fred Maclean <macleash90@gmail.com>
2021-10-07 09:41:01 +0000

    Done with member info, create member and card payment for ppt diaspora Added validatorjs npm package

--- src/index.js
+++ src/index.js
@@ -84,6 +84,7 @@ require('./routes/app.ppt.routes.js')(app);
 require('./routes/app.ppt.test.routes.js')(app);
 require('./routes/ussd.routes.js')(app);
 require('./routes/app.officer.routes')(app);
+require('./routes/app.diaspora.routes')(app);
 // Handler for 404 - Resource Not Found
 app.use((req, res, next) => {
     res.status(404).send({ message: 'We think you are lost!' });
