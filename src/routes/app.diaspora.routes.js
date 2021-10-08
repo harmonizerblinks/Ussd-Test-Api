4d8420542498acd10a7fba7b431ad7abd854ae7b
Harmony Alabi <harmonizerblinks@gmail.com>
2021-10-08 08:49:14 +0000

    Saving changes

--- src/routes/app.diaspora.routes.js
+++ src/routes/app.diaspora.routes.js
@@ -5,8 +5,8 @@ module.exports = function(app) {
     // const user = require('../middleware/verifysignup.middleware.js');
 
     // App user Login
-    app.post('/ppt/diaspora/member-info', diaspora.postMemberInfo);
-    app.post('/ppt/diaspora/create-member', diaspora.CreateMember);
-    app.post('/ppt/diaspora/card-payment', diaspora.CardPayment);
+    app.post('/ppt/diaspora/memberinfo', diaspora.postMemberInfo);
+    app.post('/ppt/diaspora/createmember', diaspora.CreateMember);
+    app.post('/ppt/diaspora/deposit/card', diaspora.CardPayment);
    
 }
