e35b4198b61d68135ad000d0c361628335ec9455
Fred Maclean <macleash90@gmail.com>
2021-10-14 00:59:19 +0000

    Done with implementation of Personal accounts Ie Savings, Withdrawal, change pin, check Balance, etc But still getting error 500 Error: Internal Server Error for savings

--- src/controllers/airteltigo/services/airtel_service.js
+++ src/controllers/airteltigo/services/airtel_service.js
@@ -74,6 +74,86 @@ exports.getInfo = async (apiurl, appid, key, mobile, callback, errorCallback) =>
         });
 }
 
-exports.getCustomerAccount = async (apiurl, merch ,  key, mobile, index ) => {
+exports.getCustomerAccount = async (apiurl, merch ,  key, mobile, index, callback, errorCallback ) => {
+    var api_endpoint = `${apiurl}getCustomerAccount/${merch}/${key}/${mobile}/${index}`;
+    var req = unirest('GET', api_endpoint)
+        .headers({
+            'Content-Type': 'application/json'
+        })
+        .end(async (resp) => {
+            // if (res.error) throw new Error(res.error); 
+            if (resp.error) {
+                // return res;
+                return await errorCallback(resp.body);
+            }
+            return await callback(resp.body);
+        });
     
+}
+
+
+exports.Deposit = async (apiurl, customer, merchant, access, callback, errorCallback) => {
+
+    var api_endpoint = `${apiurl}Deposit/${merchant}/${access.key}`;
+    var request = unirest('POST', api_endpoint)
+        .send(JSON.stringify(customer))
+        .headers({
+            'Content-Type': 'application/json'
+        })
+        .end(async (resp) => {
+            if (resp.error) {
+                return await errorCallback(resp.body);
+            }
+            return await callback(resp.body);
+
+        });
+}
+
+exports.Withdrawal = async (apiurl, customer, merchant, access, callback, errorCallback) => {
+
+    var api_endpoint = `${apiurl}Withdrawal/${merchant}/${access.key}`;
+    var request = unirest('POST', api_endpoint)
+        .send(JSON.stringify(customer))
+        .headers({
+            'Content-Type': 'application/json'
+        })
+        .end(async (resp) => {
+            if (resp.error) {
+                return await errorCallback(resp.body);
+            }
+            return await callback(resp.body);
+
+        });
+}
+
+exports.GetBalance = async (apiurl, account, merchant, callback, errorCallback) => {
+
+    var api_endpoint = `${apiurl}GetBalance/${merchant}/${account}`;
+    var request = unirest('GET', api_endpoint)
+        .headers({
+            'Content-Type': 'application/json'
+        })
+        .end(async (resp) => {
+            if (resp.error) {
+                return await errorCallback(resp.body);
+            }
+            return await callback(resp.body);
+
+        });
+}
+
+exports.getAccountTransaction = async (apiurl, merchant, access, callback, errorCallback) => {
+
+    var api_endpoint = `${apiurl}getAccountTransaction/${merchant}/${access.key}/${access.code}`;
+    var request = unirest('GET', api_endpoint)
+        .headers({
+            'Content-Type': 'application/json'
+        })
+        .end(async (resp) => {
+            if (resp.error) {
+                return await errorCallback(resp.body);
+            }
+            return await callback(resp.body);
+
+        });
 }
