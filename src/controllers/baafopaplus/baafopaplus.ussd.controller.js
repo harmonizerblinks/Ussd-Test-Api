14a43983be69c81775b5a1abf5ace634e10d4f04
Harmony Alabi <harmonizerblinks@gmail.com>
2021-10-27 17:53:26 +0000

    Saving changes

--- src/controllers/baafopaplus/baafopaplus.ussd.controller.js
+++ src/controllers/baafopaplus/baafopaplus.ussd.controller.js
@@ -375,9 +375,9 @@ menu.state('Deposit.confirm', {
         await postDeposit(data, async (result) => {
             // menu.end(JSON.stringify(result)); 
         });
-        let message = 'Payment request of amount GHC ' + amount + ' sent to your phone.';
+        let message = 'Payment request of amount GHC ' + amount + ' sent.';
         if (network == "MTN") {
-            message+="\nIf you don't get the prompt after 20 seconds, kindly dial *170# >> My Wallet >> My Approvals and approve payment"
+            message+="\nIf you don't get the prompt after 20 seconds, kindly dial *170#>>My Wallet>> My Approvals & approve payment"
         }
         menu.end(message);
     }
